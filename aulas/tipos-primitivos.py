bdff54c74162b204a4fa40bbf450307bde5493d1
Henrique Cardoso <henrique.prates.br@gmail.com>
2024-10-21 23:52:15 -0300

    criando mais exemplos de tipos primitivos

--- aulas/tipos-primitivos.py
+++
@@ -1,4 +0,0 @@
-n1 = float(input('Digite um número: '))
-n2 = float(input('Digite outro número: '))
-soma = n1 + n2
-print('A soma vale', soma)
---
+++ aulas/tipos-primitivos/exemplo002.py
@@ -0,0 +1,5 @@
+n = input("Digite um valor: ")
+print(type(n))
+print(n.isnumeric())
+print(n.isalpha())
+print(n.isalnum())
