7b50bab2dbe4ec4085508a0ae1b5fccf6e7a38f3
Henrique Cardoso <henrique.prates.br@gmail.com>
2024-10-22 08:56:11 -0300

    ex008.py

---
+++ exercicios/ex008.py
@@ -0,0 +1,4 @@
+metro = float(input('Digite o valor em metros: '))
+centimetro = metro * 100
+milimetro = metro * 1000
+print('{:.2f}m é igual a {:.2f}cm e {:.2f}mm'.format(metro, centimetro, milimetro))
