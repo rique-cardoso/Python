81d13815802b89b346237296cb604b09104baed4
Henrique Cardoso <henrique.prates.br@gmail.com>
2024-10-22 08:50:14 -0300

    ex006.py

---
+++ exercicios/ex006.py
@@ -0,0 +1,5 @@
+numero  = float(input('Digite um número: '))
+dobro = numero * 2
+triplo = numero * 3
+raiz_quadrada = numero ** (1 / 2)
+print('Dobro: {}\nTriplo: {}\n Raíz Quadrada: {:.2f}'.format(dobro, triplo, raiz_quadrada))
