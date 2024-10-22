649c9183b00a4af01dde29f8942175f800924f8c
Henrique Cardoso <henrique.prates.br@gmail.com>
2024-10-22 09:21:19 -0300

    ex011.py

---
+++ exercicios/ex011.py
@@ -0,0 +1,5 @@
+altura = float(input('Altura: '))
+largura = float(input('Largura: '))
+area = altura * largura
+tinta = area / 2
+print('Serão necessários {:.2f}L de tinta para pintar a sua parede.'.format(tinta))
