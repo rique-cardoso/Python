89cc771d289c115121fd95a11839d1e8fc43e7f4
Henrique Cardoso <henrique.prates.br@gmail.com>
2024-10-22 10:03:50 -0300

    correção ex005.py

--- exercicios/ex005.py
+++ exercicios/ex005.py
@@ -1,4 +1,9 @@
-numero = int(input('Digite um número: '))
+""" numero = int(input('Digite um número: '))
 antecessor = numero - 1
 sucessor = numero + 1
-print('Antecessor: {}\nSucessor: {}'.format(antecessor, sucessor)) 
+print('Antecessor: {}\nSucessor: {}'.format(antecessor, sucessor)) """
+
+#CORREÇÃO:
+
+numero = int(input('Digite um número: '))
+print('Analisando o valor {}, seu antecessor é {} e seu sucessor é {}.'.format(numero, (numero - 1), (numero + 1)))
