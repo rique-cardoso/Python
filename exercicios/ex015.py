645f8e10aac50a2b7d98155c476f4d9d8d41c5af
Henrique Cardoso <henrique.prates.br@gmail.com>
2024-10-25 12:33:33 -0300

    correção ex015.py

--- exercicios/ex015.py
+++ exercicios/ex015.py
@@ -1,6 +1,11 @@
-dias = int(input('Quantidade de dias alugado: '))
+""" dias = int(input('Quantidade de dias alugado: '))
 km = float(input('km rodados: '))
 valor_dias = 60 * dias
 valor_km = 0.15 * km
 valor_total = valor_dias + valor_km
-print('O valor total do aluguel é R${:.2f}'.format(valor_total))
+print('O valor total do aluguel é R${:.2f}'.format(valor_total)) """
+# CORREÇÃO:
+dias = int(input('Dias alugado: '))
+km = float(input('km rodados: '))
+valor_aluguel = (dias * 60) + (km * 0.15)
+print('O valor a total do aluguel é R${:.2f}'.format(valor_aluguel))
