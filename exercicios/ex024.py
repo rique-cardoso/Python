3575d3aab2754f8a08a8834d198686a3ab65f95d
Henrique Cardoso <henrique.prates.br@gmail.com>
2024-10-29 13:49:12 -0300

    ex024.py - correção

--- exercicios/ex024.py
+++ exercicios/ex024.py
@@ -4,3 +4,6 @@ if is_santo == 0:
     print('Essa cidade começa com SANTO')
 else:
     print('Essa cidade não começa com SANTO')
+# CORREÇÃO
+""" cidade = str(input('Em que cidade você nasceu? ')).strip()
+print(cidade[:5].upper() == 'SANTO') """
