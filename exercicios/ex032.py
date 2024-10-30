d4cda92a9dfc18bf7091d787fe4dc0e52ea6cfb7
Henrique Cardoso <henrique.prates.br@gmail.com>
2024-10-30 13:35:29 -0300

    ex032.py - correção ano bissexto

--- exercicios/ex032.py
+++ exercicios/ex032.py
@@ -6,3 +6,11 @@ if (teste1 and teste2 and teste3) or (teste1 and not teste2):
     print('Este ano é bissexto')
 else:
     print('Este não é um ano bissexto')
+
+# CORREÇÃO:
+
+""" ano = int(input('Ano: '))
+if ano % 4 == 0 and ano % 100 != 0:
+    print('O ano {} é BISSEXTO'.format(ano))
+else:
+    print('O ano {} NÃO É BISSEXTO'.format(ano)) """
