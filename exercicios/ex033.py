9cba014944e9a1d01e920f6373b26b0508ca67ad
Henrique Cardoso <henrique.prates.br@gmail.com>
2024-10-30 13:38:42 -0300

    ex033.py - correção maior e menor dentre 3 números

--- exercicios/ex033.py
+++ exercicios/ex033.py
@@ -20,3 +20,21 @@ else:
     else:
         menor = num2
 print('O maior número é {}\nO menor número é {}'.format(maior, menor))
+
+
+# CORREÇÃO:
+""" a = int(input('Número 1: '))
+b = int(input('Número 2: '))
+c = int(input('Número 3: '))
+menor = a
+if b < a and b < c:
+    menor = b
+if c < a and c < b:
+    menor = c
+maior = a
+if b > a and b > c:
+    maior = b
+if c > a and c > b:
+    maior = c
+print('O menor valor digitado foi {}'.format(menor))
+print('O maior valor digitado foi {}'.format(maior)) """
