411aea6a6e9738edc7e6fa8ed069375097d5bde8
Henrique Cardoso <henrique.prates.br@gmail.com>
2024-11-18 14:07:50 -0300

    Correção ex051 - melhorando a apresentação visual

--- exercicios/ex051.py
+++ exercicios/ex051.py
@@ -2,4 +2,5 @@ primeiro_termo = int(input('Digite o 1° Termo da P.A: '))
 razao = int(input('Digite a razão da P.A: '))
 for c in range(1, 11):
     termo = primeiro_termo + ((c - 1) * razao)
-    print('{}° termo é {}'.format(c, termo))
+    print('{}'.format(termo), end=' → ')
+print('FIM')
