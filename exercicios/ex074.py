6f097282e05015b68a9797cb617f78a18a1c6726
Henrique Cardoso <henrique.prates.br@gmail.com>
2024-12-18 17:58:47 -0300

    corrindo exercício 74

--- exercicios/ex074.py
+++ exercicios/ex074.py
@@ -1,15 +1,5 @@
 import random
 numAleatorios = (random.randint(1,10), random.randint(1,10), random.randint(1,10), random.randint(1,10), random.randint(1,10))
-maior = menor = 0
-for indice, elemento in enumerate(numAleatorios):
-    if indice == 0:
-        maior = elemento
-        menor = elemento
-    else:
-        if elemento > maior:
-            maior = elemento
-        if elemento < menor:
-            menor = elemento
-print(f'Os valores sorteados foram: {numAleatorios}')
-print(f'O maior número é {maior}')
-print(f'O menor número é {menor}')
+print(f'Os números sorteados foram: {numAleatorios}')
+print(f'O maior número é {max(numAleatorios)}')
+print(f'O menor número é {min(numAleatorios)}')
