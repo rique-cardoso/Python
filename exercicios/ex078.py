7e78426e6ff0bd78d36e122b151dd47aba4062e0
Henrique Cardoso <henrique.prates.br@gmail.com>
2024-12-19 13:08:28 -0300

    ex078.py

---
+++ exercicios/ex078.py
@@ -0,0 +1,6 @@
+lista = list()
+for i in range(0, 5):
+    lista.append(int(input('Digite um valor: ')))
+
+print(f'Maior valor digitado: {max(lista)}')
+print(f'Maior valor digitado: {min(lista)}')
