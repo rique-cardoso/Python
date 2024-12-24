624c6ae455b7da140f725a9dcc0b5a948018af9b
Henrique Prates (DELL - Aninha) <henrique.prates.br@gmail.com>
2024-12-24 20:31:02 -0300

    Correção do ex085

--- exercicios/ex085.py
+++ exercicios/ex085.py
@@ -1,4 +1,4 @@
-numeros = list()
+""" numeros = list()
 pares = list()
 impares = list()
 for c in range(0, 7):
@@ -13,4 +13,20 @@ numeros.append(pares[:])
 numeros.append(impares[:])
 for num in numeros:
     for n in num:
-        print(n, end=' ')
+        print(n, end=' ') """
+
+# CORREÇÃO
+
+num = [[], []]
+valor = 0
+for c in range(1, 8):
+    valor = int(input(f'Digite o {c}°. Valor: '))
+    if valor % 2 == 0:
+        num[0].append(valor)
+    else:
+        num[1].append(valor)
+print('=' * 60)
+num[0].sort()
+num[1].sort()
+print(f'Os valores pares digitados foram: {num[0]}')
+print(f'Os valores ímpares digitados foram: {num[1]}')
