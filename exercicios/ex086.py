ee070b1e6aae92b315104903f26018f9cfbca5b0
Henrique Prates (DELL - Aninha) <henrique.prates.br@gmail.com>
2024-12-24 20:49:01 -0300

    Correção ex086

--- exercicios/ex086.py
+++ exercicios/ex086.py
@@ -1,4 +1,4 @@
-matriz = [[], [], []]
+""" matriz = [[], [], []]
 for linha in range(3):
     for coluna in range(3):
         num = int(input(f'Digite um valor para [{linha}, {coluna}]: '))
@@ -6,4 +6,16 @@ for linha in range(3):
 for linha in matriz:
     for valor in linha:
         print(f'[  {valor:^5}  ]', end=' ')
+    print() """
+
+# CORREÇÃO
+
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+for l in range(0, 3):
+    for c in range(0, 3):
+        matriz[l][c] = int(input(f'Digite um valor para [{l}, {c}]: '))
+print('=' * 30)
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f'[{matriz[l][c]:^5}]', end='')
     print()
