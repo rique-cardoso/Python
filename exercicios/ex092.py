d2fa9a139216f471bf598e463243b7d78ba801bc
Henrique Prates (DELL - Aninha) <henrique.prates.br@gmail.com>
2024-12-27 14:08:24 -0300

    ex092 - Cadastro de trabalhador em Python

---
+++ exercicios/ex092.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+pessoa = {}
+pessoa['nome'] = input('Nome: ')
+ano_nascimento = int(input('Ano de nascimento: '))
+pessoa['idade'] = datetime.now().year - ano_nascimento
+ctps = int(input('CTPS (0 - Não tem): '))
+if ctps != 0:
+    pessoa['ctps'] = ctps
+    ano_contratacao = int(input('Ano de contratação: '))
+    pessoa['ano_aposentadoria'] = ano_contratacao + 35
+    pessoa['salario'] = float(input('Salário: R$'))
+else:
+    pessoa['ctps'] = None
+print('=' * 30)
+for chave, valor in pessoa.items():
+    print(f'{chave}: {valor}')
