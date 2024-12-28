30c509693d44e5eb38fc7760270bcbd013cdd52a
Henrique Prates (DELL - Aninha) <henrique.prates.br@gmail.com>
2024-12-28 12:13:53 -0300

    Melhorando o exercício 95 - Aprimorando Dicionários

--- exercicios/ex095.py
+++ exercicios/ex095.py
@@ -5,11 +5,15 @@ while True:
     jogador['gols'] = list()
     total_jogos = int(input('Quantidade de jogos: '))
     for i in range(0, total_jogos):
-        jogador['gols'].append(int(input(f'Quantidade de gols marcados por {jogador["nome"]} na partida {i+1}: ')))
+        jogador['gols'].append(int(input(f'   Quantidade de gols marcados por {jogador["nome"]} na partida {i+1}: ')))
     jogador['total de gols'] = sum(jogador['gols'])
     jogadores.append(jogador.copy())
     jogador.clear()
-    resposta = input('Deseja continuar? [S/N]: ').upper()
+    while True:
+        resposta = input('Deseja continuar? [S/N]: ').upper()[0]
+        if resposta in 'SN':
+            break
+        print('ERRO! Responda apenas S ou N.')
     if resposta == 'N':
         break
 print('='*40)
@@ -19,9 +23,11 @@ for index, jogador in enumerate(jogadores):
     print(f" {index} {jogador['nome']:<15} {str(jogador['gols']):<15} {jogador['total de gols']:<5}")
 print('-'*40)
 while True:
-    id_jogador = int(input('Mostrar dados de qual jogador? '))
+    id_jogador = int(input('Mostrar dados de qual jogador? (999 para parar): '))
     if id_jogador == 999:
         break
+    if id_jogador < 0 or id_jogador >= len(jogadores):
+        print(f'ERRO! Não existe jogador com o código {id_jogador}!')
     else:
         for index, jogador in enumerate(jogadores):
             if index == id_jogador:
@@ -29,8 +35,4 @@ while True:
                 for indice, gol in enumerate(jogador['gols']):
                     print(f'   No jogo {indice+1} fez {gol} gols.')
                 print('-'*40)
-            elif id_jogador < 0 or id_jogador >= len(jogadores):
-                break
-        print(f'ERRO. Não existe jogador com o código {id_jogador}. Tente novamente.')
-        print('-'*40)
 print('<< VOLTE SEMPRE >>')
