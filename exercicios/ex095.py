bb56178cc7d3a66aa0fd45c6948b145b9306177f
Henrique Prates (DELL - Aninha) <henrique.prates.br@gmail.com>
2024-12-28 11:55:01 -0300

    Terminando o exercício 95

--- exercicios/ex095.py
+++ exercicios/ex095.py
@@ -17,3 +17,20 @@ print('cod nome           gols           total')
 print('-'*40)
 for index, jogador in enumerate(jogadores):
     print(f" {index} {jogador['nome']:<15} {str(jogador['gols']):<15} {jogador['total de gols']:<5}")
+print('-'*40)
+while True:
+    id_jogador = int(input('Mostrar dados de qual jogador? '))
+    if id_jogador == 999:
+        break
+    else:
+        for index, jogador in enumerate(jogadores):
+            if index == id_jogador:
+                print(f'-- LEVANTAMENTO DO JOGADOR {jogador["nome"]}')
+                for indice, gol in enumerate(jogador['gols']):
+                    print(f'   No jogo {indice+1} fez {gol} gols.')
+                print('-'*40)
+            elif id_jogador < 0 or id_jogador >= len(jogadores):
+                break
+        print(f'ERRO. Não existe jogador com o código {id_jogador}. Tente novamente.')
+        print('-'*40)
+print('<< VOLTE SEMPRE >>')
