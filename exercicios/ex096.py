7506bdf649426ea465ca6c035c2488621e4b9212
Henrique Prates (DELL - Aninha) <henrique.prates.br@gmail.com>
2025-01-04 00:35:09 -0300

    ex097 - Um print especial

--- exercicios/ex096.py
+++ exercicios/ex096.py
@@ -2,4 +2,4 @@ def area(b, h):
     return b * h
 base = float(input('Base (b): '))
 altura = float(input('Altura (h): '))
-print(f'A área deste terreno é {area(base, altura):.2f} metros quadrados')
+print(f'A área deste terreno é {area(base, altura):.2f}m²')
---
+++ exercicios/ex097.py
@@ -0,0 +1,8 @@
+def escreva(msg):
+    qtd_carac = len(msg)
+    print('-'*qtd_carac)
+    print(msg)
+    print('-'*qtd_carac)
+escreva('Henrique')
+escreva('Milu')
+escreva('Autralobitecus')
