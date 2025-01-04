2effdec9ea8e69ba1f5bdbd13ec60893183f2b9d
Henrique Prates (DELL - Aninha) <henrique.prates.br@gmail.com>
2025-01-04 00:28:12 -0300

    Ex096 - Função que calcula a área

---
+++ exercicios/ex096.py
@@ -0,0 +1,5 @@
+def area(b, h):
+    return b * h
+base = float(input('Base (b): '))
+altura = float(input('Altura (h): '))
+print(f'A área deste terreno é {area(base, altura):.2f} metros quadrados')
