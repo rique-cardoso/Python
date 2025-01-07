21ebc2f4de181d19d2bde437bfb653d3ce8029ed
Henrique Prates (DELL - Aninha) <henrique.prates.br@gmail.com>
2025-01-07 03:15:57 -0300

    funções - exemplo012: retorno

---
+++ aulas/funcoes/exemplo012.py
@@ -0,0 +1,7 @@
+def soma (a=0, b=0, c=0):
+    s = a + b + c
+    return s
+r1 = soma(10, 20, 30)
+r2 = soma(10, 20)
+r3 = soma(10)
+print(f'O resultado das minahs somas foram: {r1}, {r2}, {r3}.')
--- aulas/funcoes/exemplo11.py
+++ aulas/funcoes/exemplo11.py
@@ -1,4 +1,4 @@
-# ESCOPO DE VARIÁVEIS - resolvendo o problema da repetição de nome de variável em um novo escopo# ESCOPO DE VARIÁVEIS
+# ESCOPO DE VARIÁVEIS - resolvendo o problema da repetição de nome de variável em um novo escopo
 def teste(b):
     global a # ===> a partir de agora, a será tratado como o 'a' de escopo global
     a = 8
