1596cec5be769f8ffd8f124130dbb47586bfd320
Henrique Prates (DELL - Aninha) <henrique.prates.br@gmail.com>
2025-01-13 13:54:27 -0300

    ex115c - Finalizando projeto

--- exercicios/ex115/main.py
+++ exercicios/ex115/main.py
@@ -7,13 +7,19 @@ if not arquivo.existe(nome_arquivo):
 while True:
     resposta = interface.menu(['Ver pessoas cadastradas', 'Cadastrar novas pessoa', 'Sair do Sistema'])
     if resposta == 1:
-        # Opção de listar um arquivo
+        # Opção de listar um arquivo.
         arquivo.ler(nome_arquivo)
     elif resposta == 2:
-        interface.cabecalho('Opção 2')
+        # Opção de cadastrar uma nova pessoa.
+        interface.cabecalho('NOVO CADASTRO')
+        nome = input('Nome: ')
+        idade = interface.leiaInt('Idade: ')
+        arquivo.cadastrar(nome_arquivo, nome, idade)
     elif resposta == 3:
+        # Opção de sair do sistema.
         interface.cabecalho('Saindo do sistema... Até logo!')
         break
     else:
+        # Digitou uma opção errada no menu.
         print('\033[31mERRO! Digite uma opção válida!\033[m')
     sleep(2)
